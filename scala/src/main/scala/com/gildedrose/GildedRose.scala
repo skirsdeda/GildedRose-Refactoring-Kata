0903aea5657c63c2d115305b890d4788c242a382
Tadas Dailyda <tadas@dailyda.com>
2020-01-23 14:59:26 +0200

    Refactoring + tests for scala

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,58 +1,45 @@
 package com.gildedrose
 
+import scala.math._
+import scala.util.matching.Regex
+
 class GildedRose(val items: Array[Item]) {
+  def updateQuality(): Unit = GildedRose.updateQuality(items)
+}
 
+object GildedRose {
+  val MinQuality = 0
+  val MaxQuality = 50
 
-  def updateQuality() {
-    for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
+  type QualityChangeFunc = (Item, Option[Int]) => Option[Int]
+
+  def atSellInRange(atSellIn: Range)(qualityChangeF: QualityChangeFunc): QualityChangeFunc =
+    (item, suggestedChange) =>
+      if (atSellIn.contains(item.sellIn)) qualityChangeF(item, suggestedChange) else suggestedChange
+
+  implicit val toFixedQualityChange: Int => QualityChangeFunc = c => (_, _) => Some(c)
+
+  val BackstagePassesR = raw"Backstage passes.*".r
+  val NotSulfurasR = raw"(?!Sulfuras)".r
+
+  val qualityClassifier: List[(Regex, QualityChangeFunc)] = List(
+    raw"Aged Brie".r -> 1,
+    BackstagePassesR -> 1,
+    BackstagePassesR -> atSellInRange(5 to 10)(2),
+    BackstagePassesR -> atSellInRange(0 to 5)(3),
+    BackstagePassesR -> atSellInRange(-1 to 0)(-MaxQuality),
+    raw"Conjured.*".r -> -2,
+    NotSulfurasR -> ((_, c) => if (c.isEmpty) Some(-1) else c),
+    NotSulfurasR -> atSellInRange(Int.MinValue to 0)((_, prevC) => prevC.map(c => if (c > 0) c else c * 2))
+  )
+
+  def updateQuality(items: Array[Item]): Unit =
+    for (item <- items)
+      qualityClassifier
+        .collect { case (regex, changeF) if regex.findPrefixMatchOf(item.name).nonEmpty => changeF }
+        .foldLeft(None: Option[Int])((prev, func) => func(item, prev))
+        .foreach { change =>
+          item.sellIn -= 1
+          item.quality = max(MinQuality, min(MaxQuality, item.quality + change))
         }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
-
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
-
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
-        }
-      }
-    }
-  }
 }
--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -3,10 +3,91 @@ package com.gildedrose
 import org.scalatest._
 
 class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
-      }
+  it should "Normal Item" in {
+    val item = itemUpdate(new Item("+5 Dexterity Vest", 10, 20))
+    item.name should equal ("+5 Dexterity Vest")
+    item.sellIn should equal (9)
+    item.quality should equal (19)
+  }
+
+  it should "Normal Item to 0" in {
+    val item = itemUpdate(new Item("+5 Dexterity Vest", 10, 12), 13)
+    item.name should equal ("+5 Dexterity Vest")
+    item.sellIn should equal (-3)
+    item.quality should equal (0)
+  }
+
+  it should "Normal Item degrades twice as fast after sellIn date" in {
+    val item = itemUpdate(new Item("+5 Dexterity Vest", 10, 20), 13)
+    item.name should equal ("+5 Dexterity Vest")
+    item.sellIn should equal (-3)
+    item.quality should equal (4)
+  }
+
+  it should "Brie" in {
+    val item = itemUpdate(new Item("Aged Brie", 2, 0))
+    item.sellIn should equal (1)
+    item.quality should equal (1)
+  }
+
+  it should "Brie through sellIn" in {
+    val item = itemUpdate(new Item("Aged Brie", 2, 0), 3)
+    item.sellIn should equal (-1)
+    item.quality should equal (3)
+  }
+
+  it should "Sulfuras" in {
+    val item = itemUpdate(new Item("Sulfuras, Hand of Ragnaros", 0, 80))
+    item.sellIn should equal (0)
+    item.quality should equal (80)
+  }
+
+  it should "Backstage passes" in {
+    val item = itemUpdate(new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10))
+    item.sellIn should equal (10)
+    item.quality should equal (11)
+  }
+
+  it should "Backstage passes 10/49" in {
+    val item = itemUpdate(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49))
+    item.sellIn should equal (9)
+    item.quality should equal (50)
+  }
+
+  it should "Backstage passes 10/48" in {
+    val item = itemUpdate(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48))
+    item.sellIn should equal (9)
+    item.quality should equal (50)
+  }
+
+  it should "Backstage passes 5/10 * 3" in {
+    val item = itemUpdate(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10), 3)
+    item.sellIn should equal (2)
+    item.quality should equal (19)
+  }
+
+  it should "Backstage passes 1/47 * 2" in {
+    val item = itemUpdate(new Item("Backstage passes to a TAFKAL80ETC concert", 1, 47), 2)
+    item.sellIn should equal (-1)
+    item.quality should equal (0)
+  }
+
+  it should "Conjured" in {
+    val item = itemUpdate(new Item("Conjured", 2, 20), 2)
+    item.sellIn should equal (0)
+    item.quality should equal (16)
+  }
+
+  it should "Conjured degrades twice as fast after sellIn" in {
+    val item = itemUpdate(new Item("Conjured", 2, 20), 3)
+    item.sellIn should equal (-1)
+    item.quality should equal (12)
+  }
+
+  private def itemUpdate(item: Item, iterations: Int = 1): Item = {
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+    for (_ <- 0 until iterations) app.updateQuality()
+    app.items(0)
+  }
 }
